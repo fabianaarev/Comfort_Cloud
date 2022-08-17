efc9820ab9bd35506da5d25acd1eb754b7b4dfae
kelseylo <kelseylo326@gmail.com>
2022-08-17 14:03:44 -0400

    updated affirm file for "affirmations" page - the connection to the actual buttons on main are not pushed

--- ComfortCloud/AffirmViewController.swift
+++ ComfortCloud/AffirmViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class AffirmViewController: UIViewController {
 
+    var affirmations:[String] = ["Your presence makes everything better",
+                                 "Your dedication is unmatched",
+                                 "Your perspective is invaluable",
+                                 "You are an inspiration to so many",
+                                 "You deserve the best and better",
+                                 "You are so kind and thoughtful",
+                                 "You look beautiful when you smile",
+                                 "You are worthy of your success",
+                                 "Your effort and persistence are astounding",
+                                 "Thank you for going the extra mile",
+                                 "You did a difficult job really well"
+]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var phraseLabel: UILabel!
+    
+    @IBAction func generateBtn(_ sender: Any) {
+        phraseLabel.text = affirmations[Int.random(in: 1..<affirmations.count)]
+    }
     /*
     // MARK: - Navigation
 
