6bfa0ecbe5f7856e60093fe3d51b21ca5540c6a7
Fabiana <fabiana.arev@gmail.com>
2022-08-18 14:16:09 -0400

    code jokes

--- ComfortCloud/JokesViewController.swift
+++ ComfortCloud/JokesViewController.swift
@@ -9,27 +9,29 @@ import UIKit
 
 class JokesViewController: UIViewController {
     
-    @IBOutlet weak var jokey: UILabel!
-    
-    @IBAction func button(_ sender: Any) {
-        
-        print("Why did the golfer bring two pairs of pants? In case he got a hole in one.")
-        
-        jokey.text = "Why did the golfer bring two pairs of pants? In case he got a hole in one."
-    }
-    
-    
-    
-
-    
-    
-    
+    var jokes:[String] = ["Why did the golfer bring two pairs of pants? In case he got a hole in one.",
+                                 "Why did the scarecrow win an award? Because he was outstanding in his field.",
+                                 "What do you call fake spaghetti? An im-pasta.",
+                                 "Why didn’t the skeleton get a prom date? He didn’t have the guts to ask anyone.",
+                                 "Can February March? No, but April May!",
+                                 "How do you wake up Lady Gaga? Poke her face!",
+                                 "Do they allow loud laughing in Hawaii? Or is it just a low ha?",
+                                 " Why do ghosts like to ride in elevators? It lifts their spirits!",
+                                 "Did you know that alligators can grow up to 15 feet? Well, I didn’t they usually only have 4!",
+                                 "What do you get when you cross a snowman and a vampire? Frostbite",
+                                 
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var labelj: UILabel!
+    
+    @IBAction func button(_ sender: Any) {
+        labelj.text = jokes[Int.random(in: 1..<jokes.count)]
     }
     
 
